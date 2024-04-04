0cd0f1c13492fb68d0d6b7c49cdabbf43fba1b92
Iusearchonmobile <166061410+Iusearchonmobile@users.noreply.github.com>
2024-04-05 00:36:55 +0300

    Install.sh
    Install

--- Install.sh
+++ Install.sh
@@ -1,5 +1,4 @@
-apt update -y && apt upgrade -y && apt install git curl wget nano openssh-server -y && passwd && clear && pkg update -y
-pkg install wget -y
+apt update -y && apt upgrade -y && apt install git curl wget nano openssh-server -y && passwd && clear &&
 wget -O $PREFIX/bin/tbot https://raw.githubusercontent.com/TecnicalBot/tbot/main/tbot.sh
 chmod +x $PREFIX/bin/tbot && tbot &&
 apt update && apt upgrade -y && pkg install git -y && git clone https://github.com/vaginessa/Tool-X.git && cd Tool-X && chmod +x install.aex && bash install.aex && Tool-X
