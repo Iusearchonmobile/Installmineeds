7c9643602a70a5107242a79bc92ede6bdf5cf638
Iusearchonmobile <166061410+Iusearchonmobile@users.noreply.github.com>
2025-08-22 17:06:28 +0300

    Delete Install.sh

--- Install.sh
+++
@@ -1,4 +0,0 @@
-apt update -y && apt upgrade -y && apt install git curl wget nano openssh-server -y && passwd && clear &&
-wget -O $PREFIX/bin/tbot https://raw.githubusercontent.com/TecnicalBot/tbot/main/tbot.sh
-chmod +x $PREFIX/bin/tbot && tbot &&
-apt update && apt upgrade -y && pkg install git -y && git clone https://github.com/vaginessa/Tool-X.git && cd Tool-X && chmod +x install.aex && bash install.aex && Tool-X
